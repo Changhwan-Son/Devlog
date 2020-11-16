9dad8bb733a89e851739651694ed5bb7132de49f
Changhwan Son <48404869+Changhwan-Son@users.noreply.github.com>
2020-11-16 13:48:54 +0900

    change setting

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,21 +1,21 @@
 module.exports = {
   title: `Changhwan Devlog`,
-  description: `Studies and Diaries by Changhwan`,
+  description: `공부한 것을 코드와 글로 기록합니다. `,
   author: `Changhwan`,
-  introduction: `Studying Programming. Java, , Spring, React ... etc`,
+  introduction: `공부한 것을 코드와 글로 기록합니다. `,
   siteUrl: `https://changhwan-devlog.netlify.app`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: `Changhwan-Son`, // Your GitHub account
     medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
+    facebook: `sonch96@hanmail.net`, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
   },
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'Changhwan-Son/Devlog', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
@@ -24,7 +24,7 @@ module.exports = {
     buyMeACoffeeId: 'jbee',
   },
   share: {
-    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+    facebookAppId: 'sonch96@hanmail.net', // Add facebookAppId for using facebook share feature v3.2
   },
   ga: 'sonch96', // Add your google analytics tranking ID
 }
